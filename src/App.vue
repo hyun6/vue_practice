4e2ad6c65ca56bda0774fc1a08e04c20bd1f6b68
moon_hyunsick <moon_hyunsick@waregram.com>
2018-09-21 20:28:25 +0900

    . 네스티드 라우터 적용 . 네임드 라우터 진행 예정

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,7 @@
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
     <h3>Routes</h3>
-    <router-link to="/page_main">Main 페이지</router-link>
-    <router-link to="/page_login">Login 페이지</router-link>
+    
     <router-view></router-view>
     <todo-footer v-on:click_event=on_click_event></todo-footer>
   </div>
@@ -27,33 +26,31 @@
 
 <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
 <script>
-
-import './main.js'
-import Vue from 'vue'
+import "./main.js";
+import Vue from "vue";
 
 var eventBus = new Vue();
 
-Vue.component('todo-footer', {
-  template : '<button v-on:click="on_click">click me</button>',
+Vue.component("todo-footer", {
+  template: '<button v-on:click="on_click">click me</button>',
   methods: {
     on_click: function() {
-      this.$emit("click_event", 'gg');
-      eventBus.$emit('on_event_bus', 100);
+      this.$emit("click_event", "gg");
+      eventBus.$emit("on_event_bus", 100);
     }
   }
 });
 
-
 var comp_todo_list = {
   template: "<div>this-is-list: {{todo_1}}</div>",
   props: {
-    todo_1:String,
-    todo_2:Number
+    todo_1: String,
+    todo_2: Number
   } // ['todo_1'] 과 같이 타입 정보가 없이 정의될 경우 잘못 사용될 우려가 있기 때문에 명시적으로 타입을 나타내는 방식으로 사용한다
 };
 
-export default {  
-  name: 'app',
+export default {
+  name: "app",
   components: {
     "todo-list": comp_todo_list
   },
@@ -63,17 +60,17 @@ export default {
     };
   },
   methods: {
-    on_click_event: function(arg){
-      console.log('on_click_event: ' + arg);
+    on_click_event: function(arg) {
+      console.log("on_click_event: " + arg);
     }
   },
   created: function() {
-    eventBus.$on('on_event_bus', function(arg){
-      console.log('on_event_bus: ' + arg );
+    eventBus.$on("on_event_bus", function(arg) {
+      console.log("on_event_bus: " + arg);
     });
   },
   beforeUpdate: function() {
-    console.log('bfUpdate');
+    console.log("bfUpdate");
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -5,37 +5,39 @@ import App from './App.vue'
 // 모듈을 통해 사용할 경우 명시적으로 use를 해줘야 함
 Vue.use(VueRouter)
 
-var page_main = {
-  template: "<div> main {{my_name}}</div>",
-  data: function() {
-    var moon_name;
-    moon_name = 'moon';
-    return {
-      my_name: moon_name
-    };
-  },
-  props: {
-    status: String
-  }
-};
-
-var page_login = {
-  template: "<div> login </div>"
+const User = {
+  template: "<div> User  <router-view></router-view> </div>"
 }
 
+const UserPost = {
+  template: "<div> User Post </div>"
+}
 
-var routes = [
-  { path: "/page_main", component: page_main },
-  { path: "/page_login", component: page_login }
-];
+const UserProfile = {
+  template: "<div> User Profile </div>"
+}
+
+var routes = [{
+  path: "/user",
+  component: User,
+  children: [{
+      path: "posts",
+      component: UserPost
+    },
+    {
+      path: "profile",
+      component: UserProfile
+    }
+  ]
+}];
 
 var router = new VueRouter({
   mode: 'history', // 브라우저 URL에서 (ex ../#/page_name) /# 을 없애줌
   routes
-});
+})
 
 new Vue({
   router,
   el: '#app',
   render: h => h(App)
-});
+})
